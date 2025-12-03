5a77f298d6a63175ab21f01fd1780bf7800c70bb
jhonManuelGil <jhonmanuelg6@gmail.com>
2025-12-03 20:24:32 -0300

    Updating: Lesson on Repetition Structures in Python.

---
+++ Python/estruturas_repeticao.py
@@ -0,0 +1,34 @@
+# a = int(input('informe um número inteiro: '))
+# print(a)
+
+# a += 1
+# print(a)
+
+# a += 1
+# print(a)
+
+
+# texto = input('informe un texto: ')
+# VOGAIS = 'AEIOU'
+
+# for letra in texto:
+#     if letra.upper() in VOGAIS:
+#         print(letra, end='')
+        
+# print()
+
+
+# for num in range(0, 50, 5):
+#    print(num, end=' ')
+
+
+ 
+while True:
+   
+    nota = int(input('Informa o número: '))
+     
+    if nota == 10:
+      break
+      print(nota)
+ 
+ 
